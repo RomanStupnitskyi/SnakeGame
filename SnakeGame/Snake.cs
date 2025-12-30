883d4ac654d1243d60b7e5ab255d581ee2c03094
Roman Stupnitskyi <roman.stupnitskyi@gmail.com>
2025-12-30 17:23:41 +0100

    Add each player colour

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -1,35 +1,38 @@
 namespace SnakeGame;
 
-public class Snake(int startX, int startY)
+public class Snake(int startX, int startY, ConsoleColor color, Direction startDir)
 {
-	public List<Position> Body { get; private set; } = [new Position(startX, startY)];
-	public Direction CurrentDirection { get; set; } = Direction.Right;
+    public List<Position> Body { get; private set; } = [new Position(startX, startY)];
+    public Direction CurrentDirection { get; set; } = startDir;
+    public ConsoleColor Color { get; private set; } = color; // Nowość: Kolor węża
 
-	public Position Head => Body.First();
+    public Position Head => Body.First();
 
-	public Position CalculateNextPosition()
-	{
-		Position head = Head;
-		Position next = new(head.X, head.Y);
+    public Position CalculateNextPosition()
+    {
+        var head = Head;
+        var next = new Position(head.X, head.Y);
 
-		switch (CurrentDirection)
-		{
-			case Direction.Left: next.X--; break;
-			case Direction.Right: next.X++; break;
-			case Direction.Up: next.Y--; break;
-			case Direction.Down: next.Y++; break;
-		}
-		return next;
-	}
+        switch (CurrentDirection)
+        {
+            case Direction.Left: next.X--; break;
+            case Direction.Right: next.X++; break;
+            case Direction.Up: next.Y--; break;
+            case Direction.Down: next.Y++; break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        return next;
+    }
 
-	public void Move(Position newHead)
-	{
-		Body.Insert(0, newHead);
-		Body.RemoveAt(Body.Count - 1);
-	}
+    public void Move(Position newHead)
+    {
+        Body.Insert(0, newHead);
+        Body.RemoveAt(Body.Count - 1);
+    }
 
-	public void Grow(Position newHead)
-	{
-		Body.Insert(0, newHead);
-	}
+    public void Grow(Position newHead)
+    {
+        Body.Insert(0, newHead);
+    }
 }
